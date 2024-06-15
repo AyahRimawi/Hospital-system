5f3f6716c8bca7b9a62f8ee5db2a26c607bb200a
ayahrimawi <ayaalrimawi406@gmail.com>
2024-06-15 17:36:56 +0300

    Hospital-system

--- app.js
+++ app.js
@@ -1,43 +1,57 @@
-document.getElementById("submit-btn").addEventListener('click',function(event) 
-{
-    event.preventDefault();
+let array=[];
 
-    let username = document.getElementById("username").value
-    let gender = document.getElementById("gender").value
-    let birthday = document.getElementById("birthday").value
-    let phone = document.getElementById("phone").value
-    let diseases = document.getElementById("diseases").value
+function Person(FullName, gender, birthday, phone, Email, diseases) {
+    this.FullName = FullName;
+    this.gender = gender;
+    this.birthday = birthday;
+    this.phone = phone;
+    this.Email = Email;
+    this.diseases = diseases;
+}
 
-    let NewPatient = 
+function render(event)
     {
-        username: username,
-        gender: gender,
-        birthday: birthday,
-        phone: phone,
-        diseases: diseases,
-    }
+        event.preventDefault();
+        let pateintForm = event.target;
+        let FullName = pateintForm.elements["FullName"].value
+        let gender = pateintForm.elements["gender"].value
+        let birthday = pateintForm.elements["birthday"].value
+        let phone = pateintForm.elements["phone"].value
+        let Email = pateintForm.elements["Email"].value
+        let diseases =pateintForm.elements["diseases"].value
+        let pateint = new Person(FullName, gender, birthday, phone, Email, diseases);
+        array.push(pateint);
+        localStorage.setItem("pateintArray", JSON.stringify(array));
+        let text = JSON.parse(localStorage.getItem("pateintArray"));
 
-    
-    render(NewPatient);
-    document.getElementById("form").reset();
-})
 
-    function render(patient)
-    {
         let card = document.createElement('div');
         card.innerHTML = `
-        <img src="pic user.png" class="card-img-top" alt="pic user" style="width: 100px; height: 100px; margin-left: 100px; margin-top: 20px;">
-        <div class="card-body">
+        <img src="pic user.png" class="card-img-top" alt="pic user" style="width: 70px; height: 70px; margin-left: 50px; margin-top: 20px;">
+        <div class="card-body>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item" style="font-weight: bold;">Gender: ${patient.username}</li>
-            <li class="list-group-item" style="font-weight: bold;">Gender: ${patient.gender}</li>
-            <li class="list-group-item" style="font-weight: bold;">Date of Birth: ${patient.birthday}</li>
-            <li class="list-group-item" style="font-weight: bold;">Phone Number: ${patient.phone}</li>
-            <li class="list-group-item" style="font-weight: bold;">Chronic Diseases: ${patient.diseases}</li>
+            <li class="list-group-item" style="font-weight: bold;margin-left: 20px;">Full Name: ${pateint.FullName}</li>
+            <li class="list-group-item" style="font-weight: bold;margin-left: 20px;">Gender: ${pateint.gender}</li>
+            <li class="list-group-item" style="font-weight: bold;margin-left: 20px;">Date of Birth: ${pateint.birthday}</li>
+            <li class="list-group-item" style="font-weight: bold;margin-left: 20px;">Phone Number: ${pateint.phone}</li>
+            <li class="list-group-item" style="font-weight: bold;margin-left: 20px;">Email: ${pateint.Email}</li>
+            <li class="list-group-item" style="font-weight: bold;margin-left: 20px;">Chronic Diseases: ${pateint.diseases}</li>
         </ul>`;
         document.getElementById("patients-card").appendChild(card);
+        
     }
+    let pateintForm = document.getElementById("form");
+    pateintForm.addEventListener("submit", render);
+
+
+
+
+
+
+
+
+
 
 
 
